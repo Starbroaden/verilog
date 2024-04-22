7bbc3d345044c0b66a2ab921c00994c752830dc9
Ivan Lavrenko <140329960+Starbroaden@users.noreply.github.com>
2024-04-22 21:32:52 +0300

    Update button.v

--- flashlight/button.v
+++ flashlight/button.v
@@ -1,12 +1,15 @@
 module button
 (
     input   clk,
-    input   button,
+    input   but,
     output  push
 );
+reg but_r;
+reg but_rr;
 always @(posedge clk) 
 begin
-	but_r <= button;
+	but_r <= but;
 	but_rr <= but_r;
 end
 assign push = but_rr&~but_r;
+endmodule
