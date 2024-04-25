d9250f757af8074d1e46ba1dc12f34df38c15e59
Ivan Lavrenko <140329960+Starbroaden@users.noreply.github.com>
2024-04-25 22:41:12 +0300

    Update flashlight.v

--- flashlight/flashlight.v
+++ flashlight/flashlight.v
@@ -1,79 +1,85 @@
-module flashlight(
-   input wire clk,
-   input wire reset,
-   input wire [13:0] switches,
-   input wire setbut,
-   output wire [6:0] digit1, digit2, digit3, digit4;
-   output wire light
+module flashlight (
+    input wire clk,
+    input wire reset,
+    input wire [13:0] switches,
+    input wire setbutt,
+    output wire flash,
+    output wire [6:0] digitcount1, digitcount2
 );
 
-reg [13:0] settimer;
-reg [15:0] timer;
-reg [13:0] counter;
-reg lightsignal;
 
-button setbutton(
-	.clk(clk), 
-	.but(setbut), 
-	.push(setpush)
+reg [15:0] CountsPerOne;
+reg [13:0] counter;
+reg [13:0] settime;
+reg [7:0] digitcnt;
+
+button setbutton (
+    .clk    (clk),
+    .but (setbutt),
+    .push   (setpush)
 );
 
 hex2seven_seg number1(
-	.hex(settimer[3:0]), 
-	.seven_seg(digit1)
+	.hex(digitcnt[3:0]),
+	.seven_seg(digitcount1)
 );
 
 hex2seven_seg number2(
-	.hex(settimer[7:4]), 
-	.seven_seg(digit2)
+	.hex(digitcnt[7:4]),
+	.seven_seg(digitcount2)
 );
-
-hex2seven_seg number3(
-	.hex(settimer[11:8]), 
-	.seven_seg(digit3)
-);
-
-hex2seven_seg number4(
-	.hex({2'b0, settimer[13:12]}), 
-	.seven_seg(digit4)
-);
-
 
 
 always @(posedge clk) 
 begin
-	if (~reset)
+    if (~reset) 
 	begin
-		settimer <= 14'h0;
-	end
-	else begin
-		if (setpush)
-		begin
-			settimer <= switches;
-		end		
-	end
+        settime <= 0;
+    end 
+	
+	else if (setpush)
+	begin
+        settime <= switches;
+    end
 end
-
-
 
 
 always @(posedge clk) 
 begin
-		if(timer >= 50000)
-		begin
-			timer <= 0;
-			counter <= counter + 1;
-		end
-		
-		if (counter == settimer)
-		begin 
-			lightsignal <= (lightsignal)? 1'b0 : 1'b1;
-			counter <= 14'h0;
-		end
+    if (~reset) 
+	begin
+        CountsPerOne <= 0;
+    end
+	
+	else 
+	begin 
+		CountsPerOne <= (CountsPerOne == 16'd49_999)? 0 : CountsPerOne + 1;
+	end
 end
 
 
+always @(posedge clk) 
+begin
+    if (~reset) 
+	begin
+        counter <= 0;
+		  digitcnt <= 0;
+    end 
+	
+	
+	else if (counter == settime) 
+	begin
+        counter <= 0;
+		  digitcnt <= (settime != 0)? digitcnt + 1 : 0;
+    end
+	 
+	else if (CountsPerOne == 16'd49_999) 
+	begin
+        counter <= counter + 1;
+    end 
+end
 
-assign light = lightsignal;
- 
+assign flash = ((counter << 1) < settime);
+
+
 endmodule
