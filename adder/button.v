bf148c52e81f9ff5615338b8432afa5c75a86cf4
Ivan Lavrenko <140329960+Starbroaden@users.noreply.github.com>
2024-04-25 22:43:19 +0300

    Add files via upload

---
+++ adder/button.v
@@ -0,0 +1,15 @@
+module button
+(
+    input   clk,
+    input   but,
+    output  push
+);
+reg but_r;
+reg but_rr;
+always @(posedge clk) 
+begin
+	but_r <= but;
+	but_rr <= but_r;
+end
+assign push = but_rr&~but_r;
+endmodule
