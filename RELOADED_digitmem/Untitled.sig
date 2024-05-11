5fdb2eee45cb9962ee7671b6b79c9bc1ffdcbcd5
Ivan Lavrenko <140329960+Starbroaden@users.noreply.github.com>
2024-05-11 04:53:25 +0300

    Add files via upload

---
+++ RELOADED_digitmem/Untitled.sig
@@ -0,0 +1,7 @@
+// PROMGEN: Xilinx Prom Generator K.39
+// Copyright (c) 1995-2008 Xilinx, Inc.  All rights reserved.
+
+DATE      05/10/24-19:14
+SOURCE    C:\Students\Lavrenko\digitmemory\//Untitled.mcs
+DEVICE    XC18V256
+SIGNATURE 0x003141CF
---
+++ RELOADED_digitmem/digitmemory.v
@@ -0,0 +1,74 @@
+`timescale 1ns/1ps
+
+module digitmemory (
+    input wire clk,
+    input wire reset,
+    input wire writepush,
+    input wire [2:0] switch_address,
+    input wire [7:0] switch_value,
+    output wire [7:0] indicate_led,
+	 //output wire [6:0] digit_addr,
+	 output wire [6:0] digit_number1,
+	 output wire [6:0] digit_number2
+);
+
+//wire writepush
+reg [7:0] memory [7:0];
+
+
+reg [7:0] indicating;
+/*
+button writebutton (
+    .clk    (clk),
+    .but (writebutt),
+    .push   (writepush)
+);
+
+
+hex2seven_seg address_digit(
+	.hex ({1'b0, switch_address}),
+	.seven_seg(digit_addr)
+);
+*/
+hex2seven_seg firstvalue_digit(
+	.hex (indicating[3:0]),
+	.seven_seg(digit_number1)
+);
+
+hex2seven_seg secondvalue_digit(
+	.hex (indicating[7:4]),
+	.seven_seg(digit_number2)
+);
+
+
+genvar cnt;
+generate
+	for (cnt = 0; cnt < 8; cnt = cnt + 1) begin: loop
+		always @(posedge clk)
+			if (reset)
+				memory[cnt] <= 8'd0;
+			else if (writepush)
+		begin
+			memory[cnt] <= (cnt == switch_address)? switch_value : memory[cnt];
+		end
+	end
+endgenerate
+
+
+always @(posedge clk)
+begin 
+	if (reset)
+		indicating <= 8'd0;
+	else
+		indicating <= memory[switch_address];
+end
+
+//assign indicate_led = indicating;
+
+decoder leding (
+	.number(switch_address),
+	.res(indicate_led)
+);
+
+
+endmodule
